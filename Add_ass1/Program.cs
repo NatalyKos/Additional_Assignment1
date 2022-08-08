708dde335df59e53949bdc734f33827f978310c1
Nataly K <doublejanus4@gmail.com>
2022-08-08 20:59:35 +0300

    Ass1

---
+++ Add_ass1/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("What is the answer to Life, Universe and Everything?");
+Console.Write("Enter your answer here: ");
+string input = Console.ReadLine();
+string answer = "42";
+int counter = 3;
+string secret = "So long, and thanks for all the fish";
+while(counter > 1)
+{
+    if(input == answer)
+        {
+            Console.WriteLine(secret);
+            break;
+        }
+    else
+    {
+        counter--;
+        Console.WriteLine("You are wrong. The number of tries you have left: " + counter);
+        Console.Write("Enter your answer here: ");
+        string input1 = Console.ReadLine();
+        
+        if(input1 == answer)
+        {
+            Console.WriteLine(secret);
+            break;
+        }
+        else
+        {
+        counter--;
+        Console.WriteLine("You are wrong. The number of tries you have left: " + counter);
+        Console.Write("Enter your answer here: ");
+        string big = Console.ReadLine();
+            if(big == answer)
+            {
+            Console.WriteLine(secret);
+            break;
+            }
+        }
+    }
+    
+Console.WriteLine("Try harder next time!");
+}
