dc2370cd7fe4858b25f5e172376064a5d0dfa7f1
Nataly K <doublejanus4@gmail.com>
2022-08-08 21:45:29 +0300

    Ass2

---
+++ Add_ass2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter (exit) to stop me: ");
+string input = Console.ReadLine();
+int counter = 1;
+while(counter >= 1)
+{
+    if(input == "exit")
+    {
+        Console.WriteLine("Thank you, now I can rest!");
+        break;
+    }
+        Console.WriteLine(counter++);
+        Console.Write("Enter (exit) to stop me: ");
+        string input2 = Console.ReadLine();
+        input = input2;
+
+}
