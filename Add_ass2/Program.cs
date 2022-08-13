3edeaf85d7a30757fc7eeabe5857d3c306504760
Nataly K <doublejanus4@gmail.com>
2022-08-13 12:01:17 +0300

    add ass2 - continue

--- Add_ass2/Program.cs
+++ Add_ass2/Program.cs
@@ -1,16 +1,53 @@
-﻿Console.Write("Enter (exit) to stop me: ");
+﻿string name = " ";
+Console.Write("Enter a command (for a list of commands enter help): ");
 string input = Console.ReadLine();
-int counter = 1;
-while(counter >= 1)
+while(input != "exit")
 {
-    if(input == "exit")
+    
+    switch (input)
     {
-        Console.WriteLine("Thank you, now I can rest!");
+        case "exit":
         break;
-    }
-        Console.WriteLine(counter++);
-        Console.Write("Enter (exit) to stop me: ");
-        string input2 = Console.ReadLine();
-        input = input2;
 
+        case "help":
+        Console.WriteLine("Enter exit to stop the program");
+        Console.WriteLine("Enter SetPassword to set a password");
+        Console.WriteLine("Enter Knock to knock");
+        Console.WriteLine("Enter CurrentDate to know the current date");
+        Console.Write("Hey, " + name + " " + "enter another command: ");
+        input = Console.ReadLine();
+        break;
+
+        case "SetPassword":
+        int password = ReadInt("Set a numerical password: ");
+        while(ReadInt("Hey, " + name + " " + "enter the correct password to continue: ") != password)
+        {
+            ReadInt("Hey, " + name + " " + "enter the correct password to continue: ");
+            
+        }
+        Console.Write("Hey, " + name + " " + "enter another command: ");
+        input = Console.ReadLine();
+        break;
+
+        case "Knock":
+        Console.Write("Who's there? State your name: ");
+        name = Console.ReadLine();
+        Console.Write("Hey, " + name + " " + "enter another command: ");
+        input = Console.ReadLine();
+        break;
+
+        case "CurrentDate":
+        Console.WriteLine(DateTime.Now + " " + name );
+        Console.Write("Hey, " + name + " " + "enter another command: ");
+        input = Console.ReadLine();
+        break;
+        
+    }
+    
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
